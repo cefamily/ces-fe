150f68e6a266e7f860198a36a7389c764eba7457
uccu <418667631@qq.com>
2016-07-25 22:22:05 +0800

    add mine and access file

--- config.js
+++ config.js
@@ -4,10 +4,10 @@
 module.exports = {
     'HOST':false,
     'ACCESS':[
-        'html','htm','js','css','jpg','png','gif','ico','txt','xml','json','shtml'
+        'html','htm','js','css','jpg','png','gif','ico','txt','xml','json','shtml','ttf','svg','eot','woff','woff2'
     ],
     'EXPIRES':[
-         'html','htm','js','css','jpg','png','gif','ico','txt','xml','json'
+         'html','htm','js','css','jpg','png','gif','ico','txt','xml','json','ttf','svg','eot','woff','woff2'
     ],
     'EXPIRES_MAXAGE':3600000,
     'COMPILE':[
