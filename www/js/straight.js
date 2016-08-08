c1d664b854d4cf0dbd34b5124220bf259b3ddbef
uccu <DuanYaFei@jyliMac.local>
2016-08-08 16:35:56 +0800

    添加图片同步顺序加载+回调函数

--- www/js/straight.js
+++ www/js/straight.js
@@ -1,25 +1,27 @@
 "use strict";
 !function(){
-    let z = window.straight,w = class{
-        constructor(x){
-            this.list = [];this.state = 0;
-            if(typeof x == 'function')this.add(x);
-        }
-        add(x){
-            this.list.push(x);return this;
-        }
-        run(y){
-            if(this.list.length==0)this.run = 0;else{
-                this.state = 1;
-                let z = this.list.shift();
-                z(this,y);
-            }
-            return this
-        }
-        get noConflict(){
-            window.straight = z;
-            return w;
-        }
+    let z = window.SYNC,w = class{
+        constructor(x){this.list = [];if(typeof x == 'function')this.add(x)}
+        add(x){if(this.list)this.list.push(x);return this}
+        run(y){if(this.list && this.list.length!=0)this.list.shift()(this,y);return this}
+        get noConflict(){window.SYNC = z;return w}
     };
-    window.straight = w;
-}();
+    window.SYNC = w;
+    window.loadPic = function(src,callback,inner){
+        if(typeof src != 'object'){
+            if(typeof src != 'string'){console.error('参数错误');return}
+            src=[{src:src,callback:callback}]
+        }else inner = callback;
+        let k = new SYNC,n=0,error=e=>console.error('图片加载失败');
+        for(let d in src){
+            let s = src[d];
+            if(typeof s!='object' || !s.src){console.warn('IMG OBJECT , TYPE ERROR');continue;}n++;
+            k.add(()=>{
+                let img=new Image();img.addEventListener('error',error);img.addEventListener('load',e=>{
+                    if(s.callback && typeof s.callback == 'function')s.callback(img,k);if(!inner)k.run()
+                });img.src = s.src
+            })
+        }k.run();return n
+    }
+}()
+
