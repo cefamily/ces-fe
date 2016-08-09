6ea17a249386ce203599502c539e435b51280359
uccu <DuanYaFei@jyliMac.local>
2016-08-09 10:49:29 +0800

    index调整

--- www/js/Common.js
+++ www/js/Common.js
@@ -1,8 +1,8 @@
 "use strict";
 (()=>{
     $.ajaxSettings.xhrFields={withCredentials: true};
-    window.host="http://api.oniicyann.baka/CES/";
-    //window.host="http://233hd.com/ces/index.php/";
+    //window.host="http://api.oniicyann.baka/CES/";
+    window.host="http://233hd.com/ces/";
 
     let z = location.search.split('-');
     window.GET = {};
