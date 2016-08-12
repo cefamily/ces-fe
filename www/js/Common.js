419bf9f9638ff71a39cdf4b5fa8d55b0f6eed72b
uccu <DuanYaFei@jyliMac.local>
2016-08-12 17:20:01 +0800

    添加职位显示

--- www/js/Common.js
+++ www/js/Common.js
@@ -3,7 +3,7 @@
     $.ajaxSettings.xhrFields={withCredentials: true};
     //window.host="http://api.oniicyann.baka/CES/";
     //window.host="http://233hd.com/ces/";
-    window.host="http://c.baka/index.php/";
+    window.host="http://d.baka/index.php/";
 
     let z = location.search.split('-');
     window.GET = {};
